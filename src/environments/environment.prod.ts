c627f11d86a4cb88e8def918b0961e197ff0cd5a
Rodrigo Pereira <rodrigopereira.development@gmail.com>
2026-03-14 18:17:55 -0300

    remove API keys do repositório

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  geminiKey: 'AIzaSyBSgQ2xJI7fOe28Y7881bqwIZR-7oaHUXk'
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  geminiKey: 'AIzaSyBSgQ2xJI7fOe28Y7881bqwIZR-7oaHUXk'
-};
