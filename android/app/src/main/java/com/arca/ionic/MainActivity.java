cc395ab73c1523b74d8bb2109e7ccae7fb89589f
RODRIGO PEREIRA <rodrigopereira.development@gmail.com>
2026-05-23 23:37:59 -0300

    feat: adicionando plataforma android nativa

---
+++ android/app/src/main/java/com/arca/ionic/MainActivity.java
@@ -0,0 +1,5 @@
+package com.arca.ionic;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.arca.ionic',
+  appName: 'arca-ionic',
+  webDir: 'www'
+};
+
+export default config;
